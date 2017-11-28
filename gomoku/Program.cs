79f304b11d4f8695b9385c1e2ea9a4b386646336
peauc <clement.peau@epitech.eu>
2017-11-28 16:44:30 +0100

    Changed project organisation // GIT INIT

---
+++ gomoku/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gomoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
