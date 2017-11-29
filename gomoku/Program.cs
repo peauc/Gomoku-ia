520d6fc4b1717667731bc60675a53e6b32071186
Clément Péau <clement.peau@epitech.eu>
2017-11-29 15:28:17 +0100

    Trying git integration in visual studio 2017

--- gomoku/Program.cs
+++ gomoku/Program.cs
@@ -7,6 +7,7 @@ namespace gomoku
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Trying git");
         }
     }
 }
