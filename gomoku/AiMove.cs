e8dcba60ac0dce9c13b5c342809d1ba5da205c89
Romain Samuel <romain.samuel@epitech.eu>
2017-12-01 13:10:30 +0100

    merge

---
+++ gomoku/AiMove.cs
@@ -0,0 +1,23 @@
+﻿namespace Gomoku
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class AiMove : Move
+    {
+        public AiMove(uint x, uint y)
+            : base(x, y)
+        {
+        }
+
+        public int HeuristicScore { get; set; }
+
+        public void ComputeHeuristicScore(IHeuristicAnalysis ha, GameState state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ gomoku/Move.cs
@@ -0,0 +1,20 @@
+﻿namespace Gomoku
+{
+    public class Move
+    {
+        public Move(uint x, uint y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public uint X { get; set; }
+
+        public uint Y { get; set; }
+
+        public string ToBoardNotation()
+        {
+            return $"{'a' + this.Y}{this.X}";
+        }
+    }
+}
